b6c1788b99e8e228e1b8f9ac0d341558a5727249
rgazelot <r.gazelot@gmail.com>
2015-06-12 00:22:09 +0200

    Use the correct number for Nexmo

--- src/Charles/MessageBundle/EventListener/NexmoSubscriber.php
+++ src/Charles/MessageBundle/EventListener/NexmoSubscriber.php
@@ -11,12 +11,14 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class NexmoSubscriber implements EventSubscriberInterface
 {
     private $guzzle;
+    private $nexmoApiNumber;
     private $nexmoApiKey;
     private $nexmoApiSecret;
 
-    public function __construct(Guzzle $guzzle, $nexmoApiKey = null, $nexmoApiSecret = null)
+    public function __construct(Guzzle $guzzle, $nexmoApiNumber = null, $nexmoApiKey = null, $nexmoApiSecret = null)
     {
         $this->guzzle = $guzzle;
+        $this->nexmoApiNumber = $nexmoApiNumber;
         $this->nexmoApiKey = $nexmoApiKey;
         $this->nexmoApiSecret = $nexmoApiSecret;
     }
@@ -33,7 +35,7 @@ class NexmoSubscriber implements EventSubscriberInterface
 
     public function onMessageCreated(MessageEvent $event)
     {
-        if (null === $this->nexmoApiKey || null === $this->nexmoApiSecret) {
+        if (null === $this->nexmoApiNumber || null === $this->nexmoApiKey || null === $this->nexmoApiSecret) {
             return;
         }
 
@@ -44,7 +46,7 @@ class NexmoSubscriber implements EventSubscriberInterface
             'query'   => [
                 'api_key' => $this->nexmoApiKey,
                 'api_secret' => $this->nexmoApiSecret,
-                'from' => 'Charles',
+                'from' => $this->nexmoApiNumber,
                 'to' => $message->getReplyTo()->getIdentifier(),
                 'type' => 'text',
                 'text' => $message->getContent(),
